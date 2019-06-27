63f0926bda54d01868610788c8378b20633f5d80
Alton1998 <39479720+Alton1998@users.noreply.github.com>
2019-06-27 10:04:40 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -5,15 +5,18 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+String item;
     public void logInClicked(View view)
     {
         EditText e1=(EditText) findViewById(R.id.t1);
         EditText e2=(EditText) findViewById(R.id.t2);
+        item=e1.getText().toString();
         Log.i("Username",e1.getText().toString());
         Log.i("Password",e2.getText().toString());
+        Toast.makeText(MainActivity.this,item,+Toast.LENGTH_SHORT).show();
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
