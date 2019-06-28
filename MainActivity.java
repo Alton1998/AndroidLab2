7889e0ec8e477f1ebb406055a56b89110ed1d414
Alton1998 <39479720+Alton1998@users.noreply.github.com>
2019-06-28 10:34:42 +0530

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -8,15 +8,17 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-String item;
+String item1,item2;
     public void logInClicked(View view)
     {
         EditText e1=(EditText) findViewById(R.id.t1);
         EditText e2=(EditText) findViewById(R.id.t2);
-        item=e1.getText().toString();
+        item1=e1.getText().toString();
+        item2=e2.getText().toString();
         Log.i("Username",e1.getText().toString());
         Log.i("Password",e2.getText().toString());
-        Toast.makeText(MainActivity.this,item,+Toast.LENGTH_SHORT).show();
+        Toast.makeText(MainActivity.this,item1,+Toast.LENGTH_SHORT).show();
+        Toast.makeText(MainActivity.this,item2,+Toast.LENGTH_SHORT).show();
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
